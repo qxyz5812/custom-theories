9a08ae27f5e8a162cd26d314fe38c50029152584
qxyz5812 <30287398+qxyz5812@users.noreply.github.com>
2024-06-20 01:39:21 +0800

    更新 FractalPatterns.js

--- official/FractalPatterns.js
+++ official/FractalPatterns.js
@@ -4,13 +4,13 @@ import { BigNumber } from "./api/BigNumber";
 import { theory, QuaternaryEntry } from "./api/Theory";
 import { Utils } from "./api/Utils";
 
-var id = "fractal_patterns";
+var id = "cctest";
 var name = "Fractal Crazy";
 var description =
   "A theory that takes advantage of the growth of the 3 fractal patterns:\n Toothpick Sequence (Tₙ),\n Ulam-Warburton cellular automaton (Uₙ),\n Sierpiński triangle (Sₙ).\n\n Big thanks to Gen (gen1code) and NGZ (ngz001) for all the help and suggestions with the LaTeX.";
-var authors = "xlii";
-var version = 7;
-var releaseOrder = "6";
+var authors = "4";
+var version = 1;
+var releaseOrder = "0";
 
 requiresGameVersion("1.4.33");
 
